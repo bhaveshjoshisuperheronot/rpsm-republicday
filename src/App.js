a2464a8e13513b416f78d8588df7f7ac4ae3d8f9
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2025-01-14 16:58:40 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cards">
+        <a className="card card-1" href="https://tinyurl.com/rpsanskrutikmandal" target="_blank" rel="noreferrer">
+          <div className="cardImg"></div>
+          <div className="cardText">Click to Register for <div><strong>Cultural Events</strong></div></div>
+        </a>
+        <a className="card card-2" href="https://wa.me/+918976416038" target="_blank" rel="noreferrer">
+          <div className="cardImg"></div>
+          <div className="cardText">Click to Register your team for <div><strong>Indirect Volleyball</strong></div></div>
+        </a>
+        <a className="card card-3" href="https://wa.me/+917304120317" target="_blank" rel="noreferrer">
+          <div className="cardImg"></div>
+          <div className="cardText">Click to Register your team for <div><strong>4-Aside Football</strong></div></div>
+        </a>
+        <a className="card card-4" href="https://wa.me/+919821030121" target="_blank" rel="noreferrer">
+          <div className="cardImg"></div>
+          <div className="cardText">Click to Register your team for <div><strong>Box Cricket</strong></div></div>
+        </a>
+        <a className="card card-5" href="https://wa.me/+919819386808" target="_blank" rel="noreferrer">
+          <div className="cardImg"></div>
+          <div className="cardText">Click to Register your team for <div><strong>Direct Volleyball</strong></div></div>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
