0a5db2229f2d43e327cf1eacb99685c3281ace95
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2025-01-14 17:25:30 +0530

    text code

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
           <div className="cardImg"></div>
           <div className="cardText">Click to Register for <div><strong>Cultural Events</strong></div></div>
         </a>
-        <a className="card card-2" href="https://wa.me/+918976416038" target="_blank" rel="noreferrer">
+        <a className="card card-2" href="https://wa.me/+918976416038?text=Hi%2C%20I%20want%20to%20register%20my%20team%20for%20Indirect%20Volleyball." target="_blank" rel="noreferrer">
           <div className="cardImg"></div>
           <div className="cardText">Click to Register your team for <div><strong>Indirect Volleyball</strong></div></div>
         </a>
-        <a className="card card-3" href="https://wa.me/+917304120317" target="_blank" rel="noreferrer">
+        <a className="card card-3" href="https://wa.me/+917304120317?text=Hi%2C%20I%20want%20to%20register%20my%20team%20for%20Football." target="_blank" rel="noreferrer">
           <div className="cardImg"></div>
           <div className="cardText">Click to Register your team for <div><strong>4-Aside Football</strong></div></div>
         </a>
-        <a className="card card-4" href="https://wa.me/+919821030121" target="_blank" rel="noreferrer">
+        <a className="card card-4" href="https://wa.me/+919821030121?text=Hi%2C%20I%20want%20to%20register%20my%20team%20for%20Cricket." target="_blank" rel="noreferrer">
           <div className="cardImg"></div>
           <div className="cardText">Click to Register your team for <div><strong>Box Cricket</strong></div></div>
         </a>
-        <a className="card card-5" href="https://wa.me/+919819386808" target="_blank" rel="noreferrer">
+        <a className="card card-5" href="https://wa.me/+919819386808?text=Hi%2C%20I%20want%20to%20register%20my%20team%20for%20Direct%20Volleyball." target="_blank" rel="noreferrer">
           <div className="cardImg"></div>
           <div className="cardText">Click to Register your team for <div><strong>Direct Volleyball</strong></div></div>
         </a>
